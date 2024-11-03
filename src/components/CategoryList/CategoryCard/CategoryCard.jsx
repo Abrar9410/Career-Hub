23a99ac0e4d4961d5f878626217e71228048b517
Abrar Shahriar <abrarshahriar9410@gmail.com>
2024-11-03 14:42:47 +0600

    categories added

--- src/components/CategoryList/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryList/CategoryCard/CategoryCard.jsx
@@ -3,10 +3,12 @@
 const CategoryCard = ({category}) => {
     const {logo, category_name, availability} = category;
     return (
-        <div className="p-10 rounded-md bg-gradient-to-r from-linear1Lighter to-linear2Lighter">
-            <div className="p-4 rounded-md bg-gradient-to-r from-linear1Light to-linear2Light">
+        <div className="p-4 sm:p-8 lg:p-10 rounded-md bg-gradient-to-r from-linear1Lighter to-linear2Lighter">
+            <div className="p-4 w-max rounded-md bg-gradient-to-r from-linear1Light to-linear2Light mb-4">
                 <img src={logo} alt="logo" />
             </div>
+            <h4 className="text-primary font-extrabold md:text-xl py-2">{category_name}</h4>
+            <p className="text-secondary">{availability}</p>
         </div>
     );
 };
--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -13,12 +13,12 @@ const CategoryList = () => {
     },[])
 
     return (
-        <div className="w-10/12 mx-auto mt-32 mb-8">
-            <div className="text-center">
+        <div className="w-10/12 mx-auto mt-32">
+            <div className="text-center mb-8">
                 <h2 className="text-primary font-extrabold md:text-5xl">Job Category List</h2>
                 <p className="text-secondary pt-6">Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
                 {
                     categoryList.map(category => 
                         <CategoryCard key={category.category_name} category={category}></CategoryCard>
